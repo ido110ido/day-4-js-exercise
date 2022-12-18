e5753c7ad8b8afdd79efac9bf7b3a629a33eb4c1
Ido Atias <ido110ido@gmail.com>
2022-12-18 15:56:30 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+
+// A
+let someName = "ido";
+console.log(someName);
+someName= "Atias";
+//alert(someName);
+
+
+//B
+const randomNum = Math.random();
+const num_by_3 = randomNum * 3;
+const cal_Result = `${randomNum} * 3 = ${num_by_3}`;
+console.log(cal_Result);
+
+//C
+const favoriteNumber = 7;
+const favoriteStringed = favoriteNumber.toString();
+console.log(favoriteStringed);
+
+//D
+const favoriteNumber_ = 6;
+const halfOfFavNum = favoriteNumber_ / 2;
+let michaelsFavorite = 13;
+michaelsFavorite = 26;
+
+//E
+const numOfChildren = 2;
+const partnerName = 'ido';
+const geoLOcation = 'tel aviv';
+const jobTitle = 'full stack';
+console.log(`You will be a ${jobTitle} in ${geoLOcation}, and married to ${partnerName} with ${numOfChildren} kids.`);
+
+//F
+const birthYear = 1999;
+let randomYear = 2051;
+let gapBbtweenYears = randomYear - birthYear;
+console.log(`I will be either ${gapBbtweenYears} or ${++gapBbtweenYears} in ${randomYear}`);
